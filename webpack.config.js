04216a6c88a8964f258deda530226859abf36653
Tony Coop <tony@thewholeworldwindow.co.uk>
2016-11-28 19:22:41 +0000

    Removed reference to cheerio external lib.

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,27 @@ const BUILD_DIR = path.resolve(__dirname, 'dist')
 const APP_DIR = path.resolve(__dirname, './')
 
 const config = {
+
   entry: './main.js',
+
   devServer: {
     hot: true,
     inline: true,
     port: 7700,
     historyApiFallback: true
   },
+
   output: {
     path: BUILD_DIR,
     filename: 'react-auto-tabs.js',
     publicPath: 'http://localhost:7700/dist'
   },
+
   externals: {
-    'cheerio': 'window',
     'react/lib/ExecutionEnvironment': true,
     'react/lib/ReactContext': true
   },
+
   module: {
     loaders: [
       {
