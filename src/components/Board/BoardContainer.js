73cfb9a9c916f0a48efe12e4d4fab42f74aa84a7
Tony Coop <tony@thewholeworldwindow.co.uk>
2016-11-29 20:30:18 +0000

    Added connect to BoardContainer.

--- src/components/Board/BoardContainer.js
+++ src/components/Board/BoardContainer.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class BoardContainer extends Component {
   render () {
+    console.log(this.props)
     return (
       <div>BoardContainer</div>
     )
   }
 }
 
-export default BoardContainer
+const mapStateToProps = ({ board }) => {
+  return {
+    lanes: board.lanes
+  }
+}
+
+export default connect(mapStateToProps)(BoardContainer)
--- src/reducers/BoardReducer.js
+++ src/reducers/BoardReducer.js
@@ -1,10 +1,13 @@
 
 const INITIAL_STATE = {
-  lanes: [],
+  lanes: [
+    { id: 1, name: 'Lane 1'},
+    { id: 2, name: 'Lane 2'}
+  ],
   stories: []
 }
 
 export default (state = INITIAL_STATE, action) => {
   console.log('Board Reducer: ', state, action)
-  return {}
+  return state
 }
