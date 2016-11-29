cd372e4acb06b8234093ed0088c7e8272e67a5b6
Tony Coop <tony@thewholeworldwindow.co.uk>
2016-11-29 21:34:42 +0000

    Added more initial state for dev.

--- src/reducers/BoardReducer.js
+++ src/reducers/BoardReducer.js
@@ -1,8 +1,10 @@
 
 const INITIAL_STATE = {
   lanes: [
-    { id: 1, name: 'Lane 1'},
-    { id: 2, name: 'Lane 2'}
+    { id: 1, name: 'Lane 1' },
+    { id: 2, name: 'Lane 2' },
+    { id: 3, name: 'Lane 3' },
+    { id: 4, name: 'Lane 4' }
   ],
   stories: []
 }
