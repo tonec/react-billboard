51620f8e07259865d1d58b07e04622101aef5012
Tony Coop <tony@thewholeworldwindow.co.uk>
2017-01-04 18:54:06 +0000

    Added test helpers for a mock store and data.

---
+++ test-helpers/mockStore.js
@@ -0,0 +1,12 @@
+const mockStore = (state) => {
+  return {
+    default: () => {},
+    subscribe: () => {},
+    dispatch: () => {},
+    getState: () => {
+      return { ...state }
+    }
+  }
+}
+
+export default mockStore
---
+++ test-helpers/mockStoreData.js
@@ -0,0 +1,15 @@
+const data = {
+  board: {
+    lanes: [
+      { id: '1', name: 'Lane 1', editing: false, storyIds: ['1', '2'] },
+      { id: '2', name: 'Lane 2', editing: false, storyIds: ['3'] }
+    ]
+  },
+  lane: {
+    stories: [
+      { id: '1', title: 'Story 1', description: 'Lorem ipsum dolor sit amet.' }
+    ]
+  }
+}
+
+export default data
