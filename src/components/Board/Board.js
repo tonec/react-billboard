5476205f38becb774e086097143ef7e5e02b1d6a
Tony Coop <tony@thewholeworldwindow.co.uk>
2017-01-09 19:18:01 +0000

    Added DragAndDropContext.

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,37 +1,42 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { DragDropContext } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
+
 import LaneContainer from '../Lane/LaneContainer'
 
-const Board = (props) => {
-  return (
-    <div className='board'>
+class Board extends Component {
+  render () {
+    return (
+      <div className='board'>
 
-      <div className='board-header'>
-        <button
-          className='btn btn-primary'
-          onClick={props.addLane}
-        >Add lane</button>
+        <div className='board-header'>
+          <button
+            className='btn btn-primary'
+            onClick={this.props.addLane}
+          >Add lane</button>
+        </div>
+
+        <div className='board-body'>
+          {this.props.lanes.map(lane => {
+            return (
+              <LaneContainer
+                key={lane.id}
+                laneId={lane.id}
+                name={lane.name}
+                editing={lane.editing}
+                storyIds={lane.storyIds}
+              />
+            )
+          })}
+        </div>
+
       </div>
-
-      <div className='board-body'>
-        {props.lanes.map(lane => {
-          return (
-            <LaneContainer
-              key={lane.id}
-              laneId={lane.id}
-              name={lane.name}
-              editing={lane.editing}
-              storyIds={lane.storyIds}
-            />
-          )
-        })}
-      </div>
-
-    </div>
-  )
+    )
+  }
 }
 
 Board.propTypes = {
   addLane: PropTypes.func.isRequired
 }
 
-export default Board
+export default DragDropContext(HTML5Backend)(Board)
