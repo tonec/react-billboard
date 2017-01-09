49bbdb1489ba706a89041dfca2b4b6a774005a32
Tony Coop <tony@thewholeworldwindow.co.uk>
2017-01-09 19:19:12 +0000

    Added dnd DragSource for stories.

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -1,21 +1,45 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { DragSource } from 'react-dnd'
 
-const Story = (props) => {
-  return (
-    <div className='story'>
-      <div className='story-title'>
-        {props.title}
+import ItemTypes from '../../constants/ItemTypes'
+
+const storySource = {
+  beginDrag (props) {
+    console.log(props)
+    return {
+      storyId: props.id
+    }
+  }
+}
+
+function collect (connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  }
+}
+
+class Story extends Component {
+  render () {
+    const { title, description, connectDragSource, isDragging } = this.props
+
+    return connectDragSource(
+      <div className='story' style={{ opacity: isDragging ? 0.5 : 1 }}>
+        <div className='story-title'>
+          {title}
+        </div>
+        <div className='story-description'>
+          {description}
+        </div>
       </div>
-      <div className='story-description'>
-        {props.description}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 Story.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired
 }
 
-export default Story
+export default DragSource(ItemTypes.STORY, storySource, collect)(Story)
