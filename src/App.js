cec33d4ba2c73ebc350e12b0aceb16315dab7484
Tony Coop <tony@thewholeworldwindow.co.uk>
2017-01-11 15:01:59 +0000

    Added redux-logger middleware.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import logger from 'redux-logger'
 import reducers from './reducers/CombinedReducers'
 import { loadState, saveState } from './storage/localStorage'
 import BoardView from './views/BoardView'
@@ -10,7 +11,7 @@ import './styles/main.scss'
 
 const App = () => {
   const persistedState = loadState()
-  const store = createStore(reducers, persistedState)
+  const store = createStore(reducers, persistedState, applyMiddleware(logger()))
 
   store.subscribe(() => {
     saveState(store.getState())
