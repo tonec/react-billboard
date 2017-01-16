2c59040371b1a37d77ce787a7e74059fa762b6ce
Tony Coop <tony@thewholeworldwindow.co.uk>
2017-01-16 01:02:14 +0000

    Moved the store into it's own file to fix it breaking tests when access from a layered component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import logger from 'redux-logger'
-import rootReducer from './reducers/RootReducer'
-import { loadState, saveState } from './storage/localStorage'
 import BoardView from './views/BoardView'
-
+import store from './storage/store'
+import { saveState } from './storage/localStorage'
 import './styles/main.scss'
 
 const App = () => {
-  const persistedState = loadState()
-  const store = createStore(rootReducer, persistedState, applyMiddleware(thunk, logger()))
-
   store.subscribe(() => {
     saveState(store.getState())
   })
---
+++ src/storage/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import rootReducer from '../reducers/RootReducer'
+import { loadState } from '../storage/localStorage'
+
+const persistedState = loadState()
+const store = createStore(rootReducer, {}, applyMiddleware(thunk, logger()))
+
+export default store
