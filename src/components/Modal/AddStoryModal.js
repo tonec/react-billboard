4b6e1aa52f056ef4ac26abf964a28cb6b2f16590
Tony Coop <tony@thewholeworldwindow.co.uk>
2017-01-16 22:22:10 +0000

    Added add story form.

--- src/components/Modal/AddStoryModal.js
+++ src/components/Modal/AddStoryModal.js
@@ -3,7 +3,35 @@ import React from 'react'
 const AddStoryModal = (props) => {
   return (
     <div>
-      <h2>Add story modal</h2>
+      <form>
+
+        <div className='form-group'>
+          <label htmlFor='story-title'>Title</label>
+          <input
+            type='text'
+            className='form-control'
+            id='story-title'
+            placeholder=''
+            autoFocus={true}
+            onChange={() => {}}
+            value={'sdsdsd'}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='story-description'>Description</label>
+          <input
+            type='text'
+            className='form-control'
+            id='story-description'
+            placeholder=''
+            autoFocus={false}
+            onChange={() => {}}
+            value={'sdsdsd'}
+          />
+        </div>
+
+      </form>
     </div>
   )
 }
