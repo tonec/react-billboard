09b4c8ceab77b60fd48d621b667a5fb5a3301c44
Tony Coop <tony@thewholeworldwindow.co.uk>
2017-01-20 18:57:39 +0000

    Added tests.

---
+++ src/containers/__spec__/AddStoryModalContainer.spec.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { expect } from 'chai'
+import { mount } from 'enzyme'
+import { Provider } from 'react-redux'
+import sinon from 'sinon'
+
+import mockStore from '../../../test-helpers/mockStore'
+import mockStoreData from '../../../test-helpers/mockStoreData'
+
+import AddStoryModalContainerConnected, { AddStoryModalContainer } from '../AddStoryModalContainer'
+import AddStoryModal from '../../components/Modal/AddStoryModal'
+
+function setup () {
+  const store = mockStore(mockStoreData)
+  const props = {}
+  const wrapper = mount(
+    <AddStoryModalContainer { ...props } />
+  )
+
+  const connectedWrapper = mount(
+    <Provider store={store}>
+      <AddStoryModalContainerConnected { ...props } />
+    </Provider>
+  )
+
+  return {
+    props,
+    wrapper,
+    connectedWrapper
+  }
+}
+
+describe('<AddStoryModal />', () => {
+
+  it('should render Lane', () => {
+    const { wrapper } = setup()
+    expect(wrapper.find(AddStoryModal)).to.be.length(1)
+  })
+
+  it('should render a save story button which calls handleSubmit on click', () => {
+    const { wrapper } = setup()
+    const component = wrapper.instance()
+    const handleSubmitStub = sinon.stub(component, 'handleSubmit', () => {})
+
+    component.forceUpdate()
+    wrapper.update()
+
+    wrapper.find('#saveStory').simulate('click')
+
+    expect(handleSubmitStub.callCount).to.equal(1)
+  })
+
+  it('should render a title input which calls handleTitleChange on change', () => {
+    const { wrapper } = setup()
+    const component = wrapper.instance()
+    const handleTitleChange = sinon.stub(component, 'handleTitleChange', () => {})
+
+    component.forceUpdate()
+    wrapper.update()
+
+    wrapper.find('#storyTitle').simulate('change')
+
+    expect(handleTitleChange.callCount).to.equal(1)
+  })
+
+  it('should render a description input which calls handleDescriptionChange on change', () => {
+    const { wrapper } = setup()
+    const component = wrapper.instance()
+    const handleDescriptionChange = sinon.stub(component, 'handleDescriptionChange', () => {})
+
+    component.forceUpdate()
+    wrapper.update()
+
+    wrapper.find('#storyDescription').simulate('change')
+
+    expect(handleDescriptionChange.callCount).to.equal(1)
+  })
+
+})
