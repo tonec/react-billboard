9df1e0e87cf332ddd4b3a0f3c9992192f6588419
Tony Coop <tony@thewholeworldwindow.co.uk>
2017-01-20 15:34:24 +0000

    Added some unfinished testing boilerplate. Still a TODO.

--- src/components/Lane/__spec__/LaneDragTarget.spec.js
+++ src/components/Lane/__spec__/LaneDragTarget.spec.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { expect } from 'chai'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import TestBackEnd from 'react-dnd-test-backend'
+import { DragDropContext } from 'react-dnd'
+import { Provider } from 'react-redux'
+import mockStore from '../../../../test-helpers/mockStore'
+import mockStoreData from '../../../../test-helpers/mockStoreData'
+import LaneDragTarget from '../LaneDragTarget'
+import Story from '../../Story/Story'
+
+function wrapInTestContext (DecoratedComponent) {
+  const store = mockStore(mockStoreData)
+
+  return DragDropContext(TestBackEnd)(
+    class TestContextContainer extends Component {
+      render () {
+
+        return (
+          <Provider store={store}>
+            <DecoratedComponent { ...this.props } />
+          </Provider>
+        )
+      }
+    }
+  )
+}
+
+// TODO: Finish DnD tests
 
 describe('<LaneDragTarget />', () => {
 
+  it('uses correct opacity is applied before and after dragging begins', () => {
+    const LaneDragTargetContext = wrapInTestContext(LaneDragTarget)
+    const wrapper = mount(<LaneDragTargetContext />)
+    const manager = wrapper.get(0).getManager()
+    const backend = manager.getBackend()
+    const monitor = manager.getMonitor()
+    const div = wrapper.find(Story)
 
+    // console.log(div.getHandlerId)
+
+    // Test before drag
+    // expect(wrapper.find('div').props().style.border).to.equal('none')
+
+    // console.log(backend.simulateBeginDrag())
+    // Test during drag
+    // expect(wrapper.find('div').props().style.border).to.equal('1px solid red')
+  })
 })
