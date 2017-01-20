49e739870fa33d3e7085b9dbe63d1c30666c1f56
Tony Coop <tony@thewholeworldwindow.co.uk>
2017-01-20 16:26:48 +0000

    Added test for testing the methods passed as props.

--- src/components/Modal/__spec__/AddStoryModal.spec.js
+++ src/components/Modal/__spec__/AddStoryModal.spec.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
+import sinon from 'sinon'
 import AddStoryModal from '../AddStoryModal'
 
 function setup () {
   const props = {
-    handleTitleChange: () => {},
-    handleDescriptionChange: () => {},
-    handleSubmit: () => {},
+    handleTitleChange: sinon.spy(),
+    handleDescriptionChange: sinon.spy(),
+    handleSubmit: sinon.spy(),
     title: 'Test title',
     description: 'Test description'
   }
@@ -42,5 +43,25 @@ describe('<AddStoryModal />', () => {
     expect(wrapper.find('.modal-footer').find('button')).to.be.length(1)
     expect(wrapper.find('.modal-footer').find('button').props().type).to.equal('submit')
     expect(wrapper.find('.modal-footer').find('button').text()).to.equal('Save story')
+  })
+
+  it('should include a submit button which, when clicked, should call the handleSubmit method passed as a prop', () => {
+    const { props, wrapper } = setup()
+
+    wrapper.find('.modal-footer').find('button').simulate('click')
+
+    expect(props.handleSubmit.calledOnce).to.equal(true)
+  })
+
+  it('should call the appropriate onChange methods when typing in each fields', () => {
+    const { props, wrapper } = setup()
+    const titleInput = wrapper.find('.form-input-title')
+    const descriptionInput = wrapper.find('.form-input-description')
+
+    titleInput.find('input').simulate('change')
+    expect(props.handleTitleChange.calledOnce).to.equal(true)
+
+    descriptionInput.find('input').simulate('change')
+    expect(props.handleDescriptionChange.calledOnce).to.equal(true)
   })
 })
